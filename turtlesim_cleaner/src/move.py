7f8434ae689d08a8ed6ca0b29714a141efea4287
blackmamba699999 <78296907+blackmamba699999@users.noreply.github.com>
2021-02-01 17:16:08 +0530

    Update move.py

--- turtlesim_cleaner/src/move.py
+++ turtlesim_cleaner/src/move.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 import rospy
 from geometry_msgs.msg import Twist
-PI = 3.1415926535897
 def move():
 
     rospy.init_node('robot_cleaner', anonymous=True)
@@ -13,7 +12,7 @@ def move():
     speed = 1
     distance = 1
 
-
+#lmao y
 
 
     vel_msg.linear.x = speed
@@ -24,131 +23,138 @@ def move():
     vel_msg.angular.y = 0
     vel_msg.angular.z = 0
 
-    while not rospy.is_shutdown():
-
-        #side1
-        dist2=1.2
-        t0 = rospy.Time.now().to_sec()
-        current_distance = 0
+    #side1
+    dist2=1.2
+    t0 = rospy.Time.now().to_sec()
+    current_distance = 0
 
 
-        while(current_distance < dist2):
-
-            velocity_publisher.publish(vel_msg)
-
-            t1=rospy.Time.now().to_sec()
-
-            current_distance= speed*(t1-t0)
-
-        vel_msg.linear.x = 0
+    while(current_distance < dist2):
 
         velocity_publisher.publish(vel_msg)
 
+        t1=rospy.Time.now().to_sec()
 
+        current_distance= speed*(t1-t0)
 
-
-        #side2
-        vel_msg.linear.x=1.545085/5
-        vel_msg.linear.y=4.755283/5
-
-        t0 = rospy.Time.now().to_sec()
-        current_distance = 0
-
-        while(current_distance < distance):
-
-            velocity_publisher.publish(vel_msg)
-
-            t1=rospy.Time.now().to_sec()
-
-            current_distance= speed*(t1-t0)
-
-        vel_msg.linear.y = 0
-
-        velocity_publisher.publish(vel_msg)
-
-
-
-
-        #side3
-        vel_msg.linear.x= -4.045085/5
-        vel_msg.linear.y= 2.93892626/5
-
-        t0 = rospy.Time.now().to_sec()
-        current_distance = 0
-
-        while(current_distance < distance):
-
-            velocity_publisher.publish(vel_msg)
-
-            t1=rospy.Time.now().to_sec()
-
-            current_distance= speed*(t1-t0)
-
-        vel_msg.linear.y = 0
-
-        velocity_publisher.publish(vel_msg)
-
-
-
-
-        #side4
-        vel_msg.linear.x= -4.045085/5
-        vel_msg.linear.y= -2.93892626/5
-
-        t0 = rospy.Time.now().to_sec()
-        current_distance = 0
-
-        while(current_distance < distance):
-
-            velocity_publisher.publish(vel_msg)
-
-            t1=rospy.Time.now().to_sec()
-
-            current_distance= speed*(t1-t0)
-
-        vel_msg.linear.y = 0
-
-        velocity_publisher.publish(vel_msg)
-
-
-
-
-
-        #side5
-        vel_msg.linear.x= 1.545085/5
-        vel_msg.linear.y= -4.755283/5
-
-        t0 = rospy.Time.now().to_sec()
-        current_distance = 0
-
-        while(current_distance < distance):
-
-            velocity_publisher.publish(vel_msg)
-
-            t1=rospy.Time.now().to_sec()
-
-            current_distance= speed*(t1-t0)
-
-        vel_msg.linear.y = 0
-        vel_msg.linear.x=0
-
-        velocity_publisher.publish(vel_msg)
-
-    vel_msg.linear.x=0
-    vel_msg.linear.y=0    
+    vel_msg.linear.x = 0
 
     velocity_publisher.publish(vel_msg)
 
 
 
 
-if __name__ == '__main__':
-    try:
-        
-        move()
-        vel_msg.linear.y = 0
-        vel_msg.linear.x=0
+    #side2
+    vel_msg.linear.x=1.545085/5
+    vel_msg.linear.y=4.755283/5
+
+    t0 = rospy.Time.now().to_sec()
+    current_distance = 0
+
+    while(current_distance < distance):
 
         velocity_publisher.publish(vel_msg)
 
+        t1=rospy.Time.now().to_sec()
+
+        current_distance= speed*(t1-t0)
+
+    vel_msg.linear.y = 0
+
+    velocity_publisher.publish(vel_msg)
+
+
+
+
+    #side3
+    vel_msg.linear.x= -4.045085/5
+    vel_msg.linear.y= 2.93892626/5
+
+    t0 = rospy.Time.now().to_sec()
+    current_distance = 0
+
+    while(current_distance < distance):
+
+        velocity_publisher.publish(vel_msg)
+
+        t1=rospy.Time.now().to_sec()
+
+        current_distance= speed*(t1-t0)
+
+    vel_msg.linear.y = 0
+
+    velocity_publisher.publish(vel_msg)
+
+
+
+
+    #side4
+    vel_msg.linear.x= -4.045085/5
+    vel_msg.linear.y= -2.93892626/5
+
+    t0 = rospy.Time.now().to_sec()
+    current_distance = 0
+
+    while(current_distance < distance):
+
+        velocity_publisher.publish(vel_msg)
+
+        t1=rospy.Time.now().to_sec()
+
+        current_distance= speed*(t1-t0)
+
+    vel_msg.linear.y = 0
+
+    velocity_publisher.publish(vel_msg)
+
+
+
+
+
+    #side5
+    vel_msg.linear.x= 1.545085/5
+    vel_msg.linear.y= -4.755283/5
+
+    t0 = rospy.Time.now().to_sec()
+    current_distance = 0
+
+    distx=0.97
+
+    while(current_distance < distx):
+
+        velocity_publisher.publish(vel_msg)
+
+        t1=rospy.Time.now().to_sec()
+
+        current_distance= speed*(t1-t0)
+
+    vel_msg.linear.y = 0
+    #vel_msg.linear.x=0
+    current_distance=5000
+
+    velocity_publisher.publish(vel_msg)
+    #rospy.is_shutdown()
+    #nigga wont stop y
+    #nigga very glitchy tbh
+
+
+
+
+    vel_msg.linear.x=0
+    vel_msg.linear.y=0
+    current_distance=5000
+
+    velocity_publisher.publish(vel_msg)
+    rospy.is_shutdown() #y u do dis
+
+
+
+
+if __name__ == '__main__':
+    try:
+        #Testing our function
+        move()
+
+
     except rospy.ROSInterruptException: pass
+
